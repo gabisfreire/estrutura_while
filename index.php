2916541dc4eb985a5c1978ff188d9cad403b9ca3
gabisfreire <130668856+gabisfreire@users.noreply.github.com>
2023-09-29 15:39:23 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//declarando variáveis
+$num = 4;
+$limite = 30;
+// enquanto número for menor que 38
+while($num < $limite) {
+
+// Exibir os números
+ echo "Executando  $num <br>";
+
+// Se o número for idêntico a 24
+if($num === 24) {
+    echo "Parando";
+break;
+}
+$num += 2;
+
+}
+?>
